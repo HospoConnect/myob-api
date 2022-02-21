d5fc5a881bccf493c3cbd137d593015f70062bb6
cameronbourgeois <cameronmbourgeois@gmail.com>
2022-02-21 15:28:27 +1300

    Add order item

--- lib/myob-api.rb
+++ lib/myob-api.rb
@@ -23,6 +23,7 @@ require 'myob/api/models/inventory_item'
 require 'myob/api/models/customer_payment'
 require 'myob/api/models/invoice'
 require 'myob/api/models/invoice_item'
+require 'myob/api/models/order_item'
 
 require 'myob/api/models/payroll_category'
 require 'myob/api/models/wage'
